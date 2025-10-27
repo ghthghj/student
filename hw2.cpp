45f56c8d4e4fb1a43210ba11bd0f8f437f6d49a7
ghthghj <s1140337@dmhs.kh.edu.tw>
2025-10-27 16:22:20 +0800

    Add files via upload

---
+++ hw2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    int age = 15;
+    double height = 178.5;
+    std::string name = "william";
+    bool is_Student = false;  
+    std::cout << "姓名: "<< name << std::end1;
+    std::cout << "身高: "<< height << std::end1;
+    std::cout << "年齡: "<< age << std::end1;
+    std::cout << "是否為學生: "<< is_Student << std::end1;
+    
+    
+    return 0;
+}
